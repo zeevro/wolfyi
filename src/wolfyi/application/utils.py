7c406f3319353700c9d1cecfb7ff6bbc2f182840
Zeev Rotshtein <zeevro@gmail.com>
2020-05-22 16:49:17 +0300

    Escape url inputs

--- src/wolfyi/application/utils.py
+++ src/wolfyi/application/utils.py
@@ -1,5 +1,7 @@
 import re
 
+from flask import escape
+
 
 VALID_EMAIL_RE = re.compile(r'''^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$''')
 
@@ -9,7 +11,7 @@ def is_valid_email(email):
 
 
 def normalize_url_input(url):
-    url = url.strip()
+    url = escape(url.strip())
     if not url:
         return None
     if '://' not in url:
